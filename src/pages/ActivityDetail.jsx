e2fb30a46ea838ffe2022fea96e588c8270fe2c6
Shao Doo <ahdoo.ling010519@gmail.com>
2023-01-10 09:50:23 +0800

    Define function for ActivityDetail

---
+++ src/pages/ActivityDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ActivityDetail() {
+    return(
+        <div className="ActivityDetail">
+            <h1>Activity Detail</h1>
+        </div>
+    )
+}
+
+export default ActivityDetail
