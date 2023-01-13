b0e44e8b3eb1d054c0dc043b9393ffb275e79ba6
chong-zhanhang <chongzhanhang@hotmail.com>
2023-01-13 23:23:53 +0800

    Refined navigation links for activities & opp form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import ActivitiesList from './pages/ActivitiesList';
+import CreateActivity from './pages/CreateActivity';
+import Activities_StuOrgView from './pages/Activities_StuOrgView';
+import Opportunities_BhepaView from './pages/Opportunities_BhepaView';
 import Opportunities from './pages/Opportunities';
 import InfoHub from './pages/InfoHub';
 import Feedback from './pages/Feedback';
@@ -14,8 +17,8 @@ function App() {
       <Navbar />
         <Routes> {/** outside Routes: display for all pages */}
           <Route path='/' element ={<Home />} />
-          <Route path='/ActivitiesList' element ={<ActivitiesList />} />
-          <Route path='/Opportunities' element ={<Opportunities />} />
+          <Route path='/Activities' element ={<Activities_StuOrgView />} />
+          <Route path='/Opportunities' element ={<Opportunities_BhepaView />} />
           <Route path='/InfoHub' element ={<InfoHub />} />
           <Route path='/Feedback' element ={<Feedback />} />
         </Routes>
--- src/pages/EditActivity.jsx
+++ src/pages/EditActivity.jsx
@@ -3,13 +3,6 @@ export default function EditActivity() {
     return(
         <div>
             <div className='w-full bg-white py-16 lg:px-16 px-5'>
-                <div className="flex justify-center">
-                    <select className="w-96 h-16 mb-20 text-xl border-orange-500 border-4 rounded-lg text-black focus:ring-4 focus:outline-none font-medium px-4 py-2.5 text-center inline-flex items-center" name="action" id="action">
-                        <option disabled selected>Choose your action</option>
-                        <option value="Create"><a href="/CreateActivity"></a>Create Activity</option>
-                        <option value="Edit"><a href="/EditActivity"></a>Edit Activity</option>
-                    </select>
-                </div>
                 <div className='lg:mx-0 mx-8 md:px-10 px-4 md:py-10 py-5 bg-slate-200 md:rounded-3xl rounded-2xl'>
                     <form>
                         <label className='block mb-7 mx-4 text-sm'>
