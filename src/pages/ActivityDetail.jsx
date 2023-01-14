dd9da5b459f8ec035702ef56200fe9c443ba3d0a
Shao Doo <ahdoo.ling010519@gmail.com>
2023-01-14 10:49:49 +0800

    Skeleton of Activity Detail page

--- src/pages/ActivityDetail.jsx
+++ src/pages/ActivityDetail.jsx
@@ -3,10 +3,16 @@ import React from "react";
 function ActivityDetail() {
     return(
         <div className="ActivityDetail">
-            <div className='text-black text-center bg-white'>
-                <div>
-                    
-                </div>
+            <div className='text-black bg-white'>
+                <h1 className="text-4xl text-center font-medium p-5">Youth Entreprenuer Success 2022</h1>
+                <h2 className="text-2xl font-normal pl-5 pb-3">Venue: </h2>
+                <h2 className="text-2xl font-normal pl-5 pb-3">Date: </h2>
+                <h2 className="text-2xl font-normal pl-5 pb-3">Link: </h2>
+                <h2 className="text-2xl font-normal pl-5 pb-3">Start Time: </h2>
+                <h2 className="text-2xl font-normal pl-5 pb-3">End Time: </h2>
+            </div>
+            <div className="p-5 bg-white">
+                <img className="rounded-md mx-auto" src="https://placeimg.com/400/225/arch" alt="Poster"/>
             </div>
         </div>        
     )
