6d57e869c346a6af1dad311e5dd5e04af1ae6ab6
Shao Doo <ahdoo.ling010519@gmail.com>
2023-01-14 10:30:47 +0800

    Add arrow svg in Read more button

--- src/pages/Opportunities.jsx
+++ src/pages/Opportunities.jsx
@@ -14,7 +14,10 @@ function Opportunities() {
               <p className="leading-5">Please join this if you intersted in Entreprenuer</p>
               <div className="card-actions justify-end">
                 <Link to='/OpportunityDetail'>
-                  <button  className="btn btn-sm">Read more</button>
+                  <button  className="btn btn-sm">
+                    Read more
+                    <svg class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                  </button>
                 </Link>
               </div>
             </div>
