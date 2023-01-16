6c3ce9f5c58c8cc52e5bcfd1a9d36dd5bbf02e00
Shao Doo <ahdoo.ling010519@gmail.com>
2023-01-16 09:55:27 +0800

    update collection to activity and activity detail page

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -5,16 +5,16 @@ import { getDocs, collection } from "@firebase/firestore";
 
 function Activities() {
 
-  const [activities, setActivities] = useState([]);
+  const [data, setData] = useState([]);
 
   const fetchPost = async () => {
        
-    await getDocs(collection(firestore, "feedback"))
+    await getDocs(collection(firestore, "activity"))
         .then((querySnapshot)=>{               
             const newData = querySnapshot.docs
                 .map((doc) => ({...doc.data(), id:doc.id }));
-            setActivities(newData);
-            console.log(activities, newData);                
+            setData(newData);
+            console.log(data, newData);                
         })   
   }
 
@@ -32,16 +32,16 @@ function Activities() {
         
         <div className="grid lg:grid-cols-3">
         {
-              activities?.map((activity)=>(
+              data?.map((data)=>(
                 <div>
                     {/* card */}
                     <div className="card w-80 bg-base-100 shadow-2xl m-7">
-                      <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                      <figure><img src={data ? data.activityPoster : "Go to Home"} alt="Shoes" /></figure>
                       <div className="card-body text-justify">
-                        <h2 className="card-title">{activity.name}</h2>
-                        <p className="leading-5">{activity.message}</p>
+                        <h2 className="card-title">{data.activityName}</h2>
+                        <p className="leading-5">{data.activityDate}</p>
                         <div className="card-actions justify-end">
-                          <Link to='/ActivityDetail'>
+                          <Link to='/ActivityDetail' state={{data: data}}>
                             <button  className="btn btn-sm">
                               Read more
                               <svg class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
--- src/pages/ActivityDetail.jsx
+++ src/pages/ActivityDetail.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function ActivityDetail() {
+    const location = useLocation();
+    const data = location.state?.data;
     return(
         <div className="ActivityDetail">
             <div className='text-black bg-white'>
-                <h1 className="text-4xl text-center font-medium p-5">Youth Entreprenuer Success 2022</h1>
-                <h2 className="text-2xl font-normal pl-5 pb-3">Venue: </h2>
-                <h2 className="text-2xl font-normal pl-5 pb-3">Date: </h2>
-                <h2 className="text-2xl font-normal pl-5 pb-3">Link: </h2>
-                <h2 className="text-2xl font-normal pl-5 pb-3">Start Time: </h2>
-                <h2 className="text-2xl font-normal pl-5 pb-3">End Time: </h2>
+                <h1 className="text-4xl text-center font-medium p-5">{data ? data.activityName : "Go to Home"}</h1>
+                <h2 className="text-2xl font-normal pl-5 pb-3">{data ? data.activityDate : "Go to Home"}</h2>
+                <h2 className="text-2xl font-normal pl-5 pb-3">{data ? data.activityStartTime : "Go to Home"}</h2>
+                <h2 className="text-2xl font-normal pl-5 pb-3">{data ? data.activityEndTime : "Go to Home"}</h2>
+                <h2 className="text-2xl font-normal pl-5 pb-3">{data ? data.activityVenue : "Go to Home"}</h2>
+                <h2 className="text-2xl font-normal pl-5 pb-3">{data ? data.activityRegLink : "Go to Home"}</h2>
             </div>
             <div className="p-5 bg-white">
-                <img className="rounded-md mx-auto" src="https://placeimg.com/400/225/arch" alt="Poster"/>
+                <img className="rounded-md mx-auto" src={data ? data.activityPoster : "Go to Home"} alt="Poster"/>
             </div>
         </div>        
     )
