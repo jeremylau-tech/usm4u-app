8ab1b790e30303912af4caf921a9483e401a3fd1
Shao Doo <ahdoo.ling010519@gmail.com>
2023-01-19 10:19:16 +0800

    Change variable name and set collection to opportunity

--- src/pages/Opportunities.jsx
+++ src/pages/Opportunities.jsx
@@ -5,15 +5,15 @@ import { getDocs, collection } from "@firebase/firestore";
 
 function Opportunities() {
 
-  const [opportunities, setOpportunities] = useState([]);
+  const [data, setData] = useState([]);
 
   const fetchPost = async () => {       
-    await getDocs(collection(firestore, "feedback"))
+    await getDocs(collection(firestore, "opportunity"))
         .then((querySnapshot)=>{               
             const newData = querySnapshot.docs
                 .map((doc) => ({...doc.data(), id:doc.id }));
-            setOpportunities(newData);
-            console.log(opportunities, newData);                
+            setData(newData);
+            console.log(data, newData);                
         })   
   }
 
@@ -31,16 +31,16 @@ function Opportunities() {
 
         <div className="grid lg:grid-cols-3">
           {
-              opportunities?.map((opportunity)=>(
+              data?.map((data)=>(
                 <div>
                     {/* card */}
                     <div className="card w-80 bg-base-100 shadow-2xl m-7">
                       <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                       <div className="card-body text-justify">
-                        <h2 className="card-title">{opportunity.name}</h2>
-                        <p className="leading-5">{opportunity.message}</p>
+                        <h2 className="card-title">{data.opportunityName}</h2>
+                        <p className="leading-5">{data.opportunityDate}</p>
                         <div className="card-actions justify-end">
-                          <Link to='/ActivityDetail'>
+                          <Link to='/OpportunityDetail' state={{data: data}}>
                             <button  className="btn btn-sm">
                               Read more
                               <svg class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
